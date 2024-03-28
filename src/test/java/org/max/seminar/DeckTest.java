e0db4690ff4fbce6a107024d9263403b2cfe2fdd
Alexander0361 <ahalatov@mail.ru>
2024-03-28 21:21:49 +0300

    push lesson1

--- src/test/java/org/max/seminar/DeckTest.java
+++ src/test/java/org/max/seminar/DeckTest.java
@@ -1,10 +1,12 @@
 package org.max.seminar;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.max.seminar.exception.DeckEmptyException;
+import org.max.seminar.type.Ranks;
+import org.max.seminar.type.Suits;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Тест кейс для тестирования объекта колоды
@@ -26,6 +28,16 @@ public class DeckTest {
         deck = new Deck(deckService.getNewDeck().getCards());
     }
 
+    @AfterEach
+    void afterEach() {
+        System.out.println("Окончание после теста.");
+    }
+
+    @AfterAll
+    static void afterAll() {
+        System.out.println("Окончание после всех тестов.");
+    }
+
     @Test
     void checkDeckSize () {
         //given
@@ -44,4 +56,34 @@ public class DeckTest {
         //then
         Assertions.assertThrows(DeckEmptyException.class, () -> deck.getCard());
     }
+
+    @Test
+    void checkEmptyDeck() {
+        //given
+        List<Card> cards = new ArrayList<>();
+        //when
+        Deck deckEmpty = new Deck(cards);
+        //then
+        Assertions.assertThrows(DeckEmptyException.class, () -> deckEmpty.getCard());
+    }
+
+    @Test
+    void checkCardFromDeck() throws DeckEmptyException {
+        //given
+        List<Card> cards = new ArrayList<>();
+        Card card1 = new Card(Ranks.SEVEN, Suits.CLUBS);
+        Card card2 = new Card(Ranks.ACE, Suits.HEARTS);
+        cards.add(card1);
+        cards.add(card2);
+        //when
+        Deck deckWithCards = new Deck(cards);
+        Card cardFromDeck = deckWithCards.getCard();
+        //then
+        Assertions.assertAll(
+                () -> Assertions.assertEquals(card1, cardFromDeck),
+                () -> Assertions.assertEquals(0, deckWithCards.getCards().size()),
+                () -> Assertions.assertThrows(DeckEmptyException.class, () -> deckWithCards.getCard())
+        );
+    }
+
 }
